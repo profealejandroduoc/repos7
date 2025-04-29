ebbf9792e7dfb5d4db8adf0a8ca3e49204d1e2f2
El profe <al.sepulvedam@profesor.duoc.cl>
2025-04-29 11:02:07 -0400

    Servicio works!!!

---
+++ src/main/java/com/seccion7/repos7/controller/PersonaController.java
@@ -0,0 +1,18 @@
+package com.seccion7.repos7.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RequestMapping("/api/personas")
+@RestController
+public class PersonaController {
+    
+    @GetMapping
+    public String saludar() {
+        return "Holi funciona el Servicio";
+    }
+    
+}
