ef764657b0e3725815b46d95954fcbe0e0874784
El profe <al.sepulvedam@profesor.duoc.cl>
2025-05-05 11:25:34 -0400

    Crud OK

--- src/main/java/com/seccion7/repos7/service/PersonaService.java
+++ src/main/java/com/seccion7/repos7/service/PersonaService.java
@@ -1,5 +1,7 @@
 package com.seccion7.repos7.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,5 +16,28 @@ public class PersonaService {
     public Persona guardar(Persona persona){
         return personaRepository.create(persona);
        
+    }
+
+    public List<Persona> listartodas()
+    {
+        return personaRepository.readAll();
+    }
+
+    public Persona buscarporid(int id) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'buscarporid'");
+        return personaRepository.read(id);
+    }
+
+
+    public Persona modificPersona(int id, Persona personamod)
+    {
+        return personaRepository.update(id, personamod);
+    }
+
+    public String eliminarPersona(int id)
+    {
+        personaRepository.delete(id);
+        return "Persona eliminada";
     }
 }
